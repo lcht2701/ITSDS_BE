80e357ccb3a5b32604e5c1402670590c761e01f8
lcht2701 <lecanhhoangthanh@gmail.com>
2023-10-03 00:48:46 +0700

    add policy name customer admin

--- Domain/Constants/PolicyName.cs
+++ Domain/Constants/PolicyName.cs
@@ -6,5 +6,6 @@ public static class Roles
     public const string MANAGER = nameof(Role.Manager);
     public const string TECHNICIAN = nameof(Role.Technician);
     public const string ACCOUNTANT = nameof(Role.Accountant);
+    public const string CUSTOMERADMIN = nameof(Role.CustomerAdmin);
 }
 
