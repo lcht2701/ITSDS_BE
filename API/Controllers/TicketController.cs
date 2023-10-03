dc28ec2a574fedc04c89d6ea1452797e10f4c830
lcht2701 <lecanhhoangthanh@gmail.com>
2023-10-03 20:18:58 +0700

    ticket CRUD (on-going)

--- API/Controllers/TicketController.cs
+++ API/Controllers/TicketController.cs
@@ -1,4 +1,5 @@
 ﻿using API.DTOs.Requests.Tickets;
+using Domain.Constants;
 using Domain.Exceptions;
 using Domain.Models;
 using Domain.Models.Tickets;
@@ -23,6 +24,7 @@ public class TicketController : BaseController
         _teamRepository = teamRepository;
     }
 
+    //Chưa biết để role gì
     [Authorize]
     [HttpGet]
     public async Task<IActionResult> GetTickets()
@@ -31,19 +33,12 @@ public class TicketController : BaseController
         return Ok(result);
     }
 
-    [Authorize]
-    [HttpGet("{ticketId}")]
-    public async Task<IActionResult> GetTicketById(int ticketId)
+    //Dùng cho customer && CustomerAdmin
+    [Authorize(Roles = $"{Roles.CUSTOMER},{Roles.CUSTOMERADMIN}")]
+    [HttpGet("my-tickets")]
+    public async Task<IActionResult> GetTicketOfCurrentUser()
     {
-        var result = await _ticketRepository.FoundOrThrow(x => x.Id.Equals(ticketId), new NotFoundException("Ticket not found"));
-        return Ok(result);
-    }
-
-    [Authorize]
-    [HttpGet("user/{userId}")]
-    public async Task<IActionResult> GetTicketsByRequester(int requesterId)
-    {
-        var result = await _ticketRepository.WhereAsync(x => x.RequesterId.Equals(requesterId));
+        var result = await _ticketRepository.WhereAsync(x => x.Id.Equals(CurrentUserID));
         if (result.Count == 0)
         {
             throw new NotFoundException("No tickets was found for this user");
@@ -51,7 +46,30 @@ public class TicketController : BaseController
         return Ok(result);
     }
 
+    //Chưa sure
     [Authorize]
+    [HttpGet("user/{userId}")]
+    public async Task<IActionResult> GetTicketsByUser(int userId)
+    {
+        var result = await _ticketRepository.WhereAsync(x => x.RequesterId.Equals(userId));
+        if (result.Count == 0)
+        {
+            throw new NotFoundException("No tickets was found for this user");
+        }
+        return Ok(result);
+    }
+
+    //Ngoại trừ Admin + Accountant
+    [Authorize(Roles = $"{Roles.MANAGER},{Roles.TECHNICIAN},{Roles.CUSTOMER},{Roles.CUSTOMERADMIN}")]
+    [HttpGet("{ticketId}")]
+    public async Task<IActionResult> GetTicketById(int ticketId)
+    {
+        var result = await _ticketRepository.FoundOrThrow(x => x.Id.Equals(ticketId), new NotFoundException("Ticket not found"));
+        return Ok(result);
+    }
+
+    //Dành cho manager và technical để quản lý của team
+    [Authorize(Roles = $"{Roles.MANAGER},{Roles.TECHNICIAN}")]
     [HttpGet("team/{teamId}")]
     public async Task<IActionResult> GetTicketsResponsibleByTeam(int teamId)
     {
@@ -64,6 +82,18 @@ public class TicketController : BaseController
     }
 
     [Authorize]
+    [HttpPost]
+    public async Task<IActionResult> CreateTicket([FromBody] CreateTicketRequest model)
+    {
+        Ticket entity = Mapper.Map(model, new Ticket());
+        //Logic
+        entity.TicketStatus = TicketStatus.Open;
+        await _ticketRepository.CreateAsync(entity);
+        return Ok("Create Successfully");
+    }
+
+    //Chưa làm Author
+    [Authorize]
     [HttpPut("{ticketId}")]
     public async Task<IActionResult> UpdateTicketInformation(int ticketId, [FromBody]UpdateTicketRequest model)
     {
@@ -73,6 +103,7 @@ public class TicketController : BaseController
         return Accepted(entity);
     }
 
+    //Chưa làm Author
     [Authorize]
     [HttpDelete("{ticketId}")]
     public async Task<IActionResult> DeleteTicket(int ticketId, [FromBody] UpdateTicketRequest model)
@@ -81,7 +112,6 @@ public class TicketController : BaseController
         await _ticketRepository.DeleteAsync(target);
         return Accepted(target);
     }
-
 }
 
 
