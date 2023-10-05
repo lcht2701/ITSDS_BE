1d1427ccb25c7465c0e73a832e2e987bbaaeca26
lcht2701 <lecanhhoangthanh@gmail.com>
2023-10-05 19:44:50 +0700

    add upload profile with avatar

---
+++ API/DTOs/Requests/Users/UpdateProfileWithAvatarRequest.cs
@@ -0,0 +1,13 @@
+﻿using API.Mappings;
+using Domain.Constants;
+using Domain.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Requests.Users
+{
+    public class UpdateProfileWithAvatarRequest : IMapTo<User>
+    {
+        public IFormFile? AvatarImage { get; set; }
+        public UpdateProfileRequest? UpdateProfileRequest { get; set; }
+    }
+}
