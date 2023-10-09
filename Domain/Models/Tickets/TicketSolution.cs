76a60faf99fcd13475fdccb383010c3cfd125981
lcht2701 <lecanhhoangthanh@gmail.com>
2023-10-09 16:48:22 +0700

    add api get current user

---
+++ Domain/Models/Tickets/TicketSolution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.Tickets
+{
+    public class TicketSolution : BaseEntity
+    {
+        public TicketSolution() { }
+
+        public string? Title { get; set; }
+
+        public string? Content { get; set; }
+
+
+    }
+}
