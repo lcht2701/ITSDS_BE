f1fc0b4da3d34da2c551eeb66cb5c038ce0ca6ca
lcht2701 <lecanhhoangthanh@gmail.com>
2023-10-11 00:24:52 +0700

    add service for changing status of task & ticket

---
+++ Persistence/Services/Implements/StatusTrackingService.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Constants.Enums;
+using Domain.Models.Tickets;
+using Persistence.Repositories.Interfaces;
+using Persistence.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Services.Implements
+{
+    public class StatusTrackingService : IStatusTrackingService
+    {
+        private readonly IRepositoryBase<Ticket> _ticketRepository;
+        private readonly IRepositoryBase<TicketTask> _taskRepository;
+
+        public StatusTrackingService(IRepositoryBase<Ticket> ticketRepository, IRepositoryBase<TicketTask> taskRepository)
+        {
+            _ticketRepository = ticketRepository;
+            _taskRepository = taskRepository;
+        }
+
+        public async Task<bool> UpdateTicketStatusTo(int ticketId, TicketStatus newStatus)
+        {
+            try
+            {
+                var ticket = await _ticketRepository.FirstOrDefaultAsync(x => x.Id.Equals(ticketId));
+                ticket.TicketStatus = newStatus;
+                await _ticketRepository.UpdateAsync(ticket);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public async Task<bool> UpdateTaskStatusTo(int taskId, TicketTaskStatus newStatus)
+        {
+            try
+            {
+                var target = await _taskRepository.FirstOrDefaultAsync(x => x.Id.Equals(taskId));
+                target.TaskStatus = newStatus;
+                await _taskRepository.UpdateAsync(target);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Persistence/Services/Interfaces/IStatusTrackingService.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Constants.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Services.Interfaces
+{
+    public interface IStatusTrackingService
+    {
+        Task<bool> UpdateTicketStatusTo(int ticketId, TicketStatus newStatus);
+        Task<bool> UpdateTaskStatusTo(int taskId, TicketTaskStatus newStatus);
+    }
+}
