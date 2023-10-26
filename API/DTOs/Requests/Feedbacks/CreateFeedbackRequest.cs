f2ce14da980a2d486bef5ca8857797d2a89230e3
lcht2701 <lecanhhoangthanh@gmail.com>
2023-10-27 00:15:38 +0700

    fix mapping profile

--- API/DTOs/Requests/Feedbacks/CreateFeedbackRequest.cs
+++ API/DTOs/Requests/Feedbacks/CreateFeedbackRequest.cs
@@ -1,15 +1,16 @@
-﻿using System.ComponentModel;
+﻿using API.Mappings;
+using Domain.Models.Tickets;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace API.DTOs.Requests.Feedbacks
-{
-    public class CreateFeedbackRequest
-    {
-        [Required]
-        public string? Comment { get; set; }
+namespace API.DTOs.Requests.Feedbacks;
 
-        [Required]
-        [DefaultValue(false)]
-        public bool? IsPublic { get; set; }
-    }
+public class CreateFeedbackRequest : IMapTo<Feedback>
+{
+    [Required]
+    public string? Comment { get; set; }
+
+    [Required]
+    [DefaultValue(false)]
+    public bool? IsPublic { get; set; }
 }
