3a2b2b292aa450710abb36b190b2969e67f015c8
lcht2701 <lecanhhoangthanh@gmail.com>
2023-10-27 00:41:20 +0700

    fix mapping

--- API/DTOs/Requests/Feedbacks/CreateFeedbackRequest.cs
+++ API/DTOs/Requests/Feedbacks/CreateFeedbackRequest.cs
@@ -1,9 +1,11 @@
-﻿using System.ComponentModel;
+﻿using API.Mappings;
+using Domain.Models.Tickets;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs.Requests.Feedbacks
 {
-    public class CreateFeedbackRequest
+    public class CreateFeedbackRequest : IMapTo<Feedback>
     {
         [Required]
         public string? Comment { get; set; }
--- API/DTOs/Requests/Feedbacks/UpdateFeedbackRequest.cs
+++ API/DTOs/Requests/Feedbacks/UpdateFeedbackRequest.cs
@@ -1,8 +1,10 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using API.Mappings;
+using Domain.Models.Tickets;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs.Requests.Feedbacks
 {
-    public class UpdateFeedbackRequest
+    public class UpdateFeedbackRequest : IMapTo<Feedback>
     {
         public string? Comment { get; set; }
 
