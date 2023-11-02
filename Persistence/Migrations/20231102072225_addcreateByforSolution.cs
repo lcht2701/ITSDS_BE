829c684137dea708f3183f5589485dc5dca5c272
lcht2701 <lecanhhoangthanh@gmail.com>
2023-11-02 14:23:43 +0700

    add createBy for Solution

---
+++ Persistence/Migrations/20231102072225_addcreateByforSolution.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class addcreateByforSolution : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CreatedById",
+                table: "TicketSolutions",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TicketSolutions_CreatedById",
+                table: "TicketSolutions",
+                column: "CreatedById");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TicketSolutions_Users_CreatedById",
+                table: "TicketSolutions",
+                column: "CreatedById",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TicketSolutions_Users_CreatedById",
+                table: "TicketSolutions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_TicketSolutions_CreatedById",
+                table: "TicketSolutions");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "TicketSolutions");
+        }
+    }
+}
