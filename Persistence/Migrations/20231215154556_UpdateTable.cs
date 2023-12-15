5d0fd254a02fc0a437bb2293551de9b4d1661ebb
lcht2701 <lecanhhoangthanh@gmail.com>
2023-12-15 22:46:58 +0700

    update solution table

---
+++ Persistence/Migrations/20231215154556_UpdateTable.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class UpdateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AttachmentUrl",
+                table: "TicketSolutions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AttachmentUrl",
+                table: "TicketSolutions",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
