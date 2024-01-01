816c1c8e6b2f2752ce41ecf59d490520e7c1dd42
lcht2701 <lecanhhoangthanh@gmail.com>
2024-01-01 23:30:32 +0700

    update User's DTOs

---
+++ API/DTOs/Requests/Users/CreateUserOrCustomerAdmin.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DTOs.Requests.Users;
+
+public class CreateUserOrCustomerAdmin
+{
+    public CreateUserRequest UserModel{ get; set; }
+    
+    public int? CompanyId { get; set; }
+
+    public bool isCompanyAdmin { get; set; }
+
+}
--- API/DTOs/Requests/Users/UpdateUserRequest.cs
+++ API/DTOs/Requests/Users/UpdateUserRequest.cs
@@ -1,28 +1,28 @@
 ﻿using API.Mappings;
 using Domain.Constants.Enums;
 using Domain.Models;
-using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs.Requests.Users
 {
     public class UpdateUserRequest : IMapTo<User>
     {
-        public string? FirstName { get; set; }
+        public string FirstName { get; set; }
 
-        public string? LastName { get; set; }
+        public string LastName { get; set; }
 
-        public string? Email { get; set; }
+        public string Email { get; set; }
+
+        public Role Role { get; set; }
+
+        public bool IsActive { get; set; }
+
+        //Field included
+        public Gender Gender { get; set; }
+
+        public string? AvatarUrl { get; set; }
 
         public string? PhoneNumber { get; set; }
 
-        public Gender? Gender { get; set; }
-
-        public bool? IsActive { get; set; }
-
         public DateTime? DateOfBirth { get; set; }
-
-        public string? Address { get; set; }
-
-        public string? AvatarUrl { get; set; }
     }
 }
