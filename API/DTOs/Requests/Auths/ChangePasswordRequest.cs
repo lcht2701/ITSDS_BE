de3a30691173363971d3a9e1e21945760015ed60
lcht2701 <lecanhhoangthanh@gmail.com>
2024-01-12 17:10:21 +0700

    add bad request message when password is less than 6 characters

--- API/DTOs/Requests/Auths/ChangePasswordRequest.cs
+++ API/DTOs/Requests/Auths/ChangePasswordRequest.cs
@@ -4,11 +4,11 @@ namespace API.DTOs.Requests.Auths
 {
     public class ChangePasswordRequest
     {
-        public string? CurrentPassword { get; set; }
+        public string CurrentPassword { get; set; }
 
-        public string? NewPassword { get; set; }
+        public string NewPassword { get; set; }
 
-        public string? ConfirmNewPassword { get; set; }
+        public string ConfirmNewPassword { get; set; }
 
     }
 }
