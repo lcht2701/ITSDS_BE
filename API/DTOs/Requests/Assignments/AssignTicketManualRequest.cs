56d28de5a5625bdbd3c4689fdb303f8406797d5e
lcht2701 <lecanhhoangthanh@gmail.com>
2024-01-14 15:48:35 +0700

    add get assignment by ticket + fix assignment logic

--- API/DTOs/Requests/Assignments/AssignTicketManualRequest.cs
+++ API/DTOs/Requests/Assignments/AssignTicketManualRequest.cs
@@ -6,8 +6,10 @@ namespace API.DTOs.Requests.Assignments
 {
     public class AssignTicketManualRequest : IMapTo<Assignment>
     {
-        public int? TechnicianId { get; set; }
+        [Required]
+        public int TechnicianId { get; set; }
 
-        public int? TeamId { get; set; }
+        [Required]
+        public int TeamId { get; set; }
     }
 }
