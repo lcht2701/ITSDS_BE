aa98028f5a508062c47fc68fd090d8b290388bb7
lcht2701 <lecanhhoangthanh@gmail.com>
2024-01-16 22:04:12 +0700

    fix service get totalPages

--- API/Validations/Services/UpdateServiceValidator.cs
+++ API/Validations/Services/UpdateServiceValidator.cs
@@ -7,14 +7,8 @@ namespace API.Validations.Services
     {
         public UpdateServiceValidator()
         {
-            RuleFor(x => x.Amount).NotNull();
-            RuleFor(x => x.Type)
-                .NotEmpty().WithMessage("Type is required.");
             RuleFor(x => x.Description)
                 .MaximumLength(100);
-            RuleFor(x => x.Amount)
-                .NotEmpty().WithMessage("Amount is required.")
-                .GreaterThanOrEqualTo(0).WithMessage("Amount should be greater than or equal to 0.");
         }
     }
 }
